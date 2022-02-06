c6685dbf0c82e73ff4516653362108b21eb3f213
carneyb3592 <ben.carney3@gmail.com>
2022-02-05 22:27:13 -0800

    2 hours in

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+    
+  );
+}
+const author = 'Author';
+const Book = () =>{
+  const title = 'Black Heros'
+  return <article>
+    <img src="https://images-na.ssl-images-amazon.com/images/I/51LHis3FzVL._AC_SX184_.jpg" alt=""/>
+    <h1>{title}</h1>
+    <h4 style ={{ color:'#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>{author}</h4>
+  </article>
+}
+
+// const Greeting = () =>{
+//   return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'));
+// };
+
+ReactDom.render(<BookList/>,document.getElementById('root'));
