888a6d6ade8bc912596edd3bb869a743c78c6ee4
carneyb3592 <ben.carney3@gmail.com>
2022-02-11 19:46:20 -0800

    Yuh

--- src/index.js
+++ src/index.js
@@ -3,31 +3,44 @@ import ReactDom from 'react-dom';
 import './index.css';
 
 
+const books = [
+ {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51LHis3FzVL._AC_SX184_.jpg",
+  title:'Black Heros',
+  author: 'Author'
+ },
+ {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51l34Xy8mwL._AC_SX184_.jpg",
+  title:'Little Blue Trucks Valentine',
+  author: 'Alice'
+  }
+]
+
+ 
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book)=>{
+        return (
+          <Book book={book}></Book>
+        );
+      })}
     </section>
     
   );
 }
-const author = 'Author';
-const Book = () =>{
-  const title = 'Black Heros'
+
+const Book = (props) => {
+  const { img, title, author} = props.book;
   return <article>
-    <img src="https://images-na.ssl-images-amazon.com/images/I/51LHis3FzVL._AC_SX184_.jpg" alt=""/>
+    <img src={img} alt=""/>
     <h1>{title}</h1>
     <h4 style ={{ color:'#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>{author}</h4>
+    <p>{children}</p>
   </article>
 }
 
-// const Greeting = () =>{
-//   return React.createElement('div', {}, React.createElement('h1', {}, 'hello world'));
-// };
+
 
 ReactDom.render(<BookList/>,document.getElementById('root'));
